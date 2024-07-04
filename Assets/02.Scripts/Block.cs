1a90e4a9620adb75a972015aa90e567898e0922a
SICKYANG <hibin7014@gmail.com>
2024-07-04 15:25:36 +0900

    밥먹고하죠

--- Assets/02.Scripts/Block.cs
+++ Assets/02.Scripts/Block.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+ 
 public class Block : MonoBehaviour
 {
-
-   
-    private void OnCollisionExit2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
